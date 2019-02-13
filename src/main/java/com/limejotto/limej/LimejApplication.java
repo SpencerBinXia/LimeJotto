fde1fca61330691851315f51ab2a218aee14afd7
Spencer X <spencerbinxia@gmail.com>
2019-02-13 18:13:55 -0500

    Lime Jotto Graven AWS Initial Commit

---
+++ src/main/java/com/limejotto/limej/LimejApplication.java
@@ -0,0 +1,14 @@
+package com.limejotto.limej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimejApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimejApplication.class, args);
+    }
+
+}
+
