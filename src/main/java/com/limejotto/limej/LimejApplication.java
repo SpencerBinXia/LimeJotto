3fea13ed890d0c6d1b060184f19b19537633cca3
Spencer X <spencerbinxia@gmail.com>
2019-02-13 22:22:43 -0500

    Commit 1:
    Project is runnable. Current SQL/cloud dependencies are commented out in build.gradle

--- src/main/java/com/limejotto/limej/LimejApplication.java
+++ src/main/java/com/limejotto/limej/LimejApplication.java
@@ -1,6 +1,7 @@
 package com.limejotto.limej;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
---
+++ src/main/java/com/limejotto/limej/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.limejotto.limej.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String home(Model model)
+    {
+        return "index";
+    }
+}
