0a01ab4480962e13e68ee52fc2a4a24bacc298e6
Spencer X <spencerbinxia@gmail.com>
2019-02-17 19:37:31 -0500

    Commit: Skeleton templates

--- src/main/java/com/limejotto/limej/controller/IndexController.java
+++ src/main/java/com/limejotto/limej/controller/IndexController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping(method= RequestMethod.GET)
+    @GetMapping
     public String home(Model model)
     {
         return "index";
