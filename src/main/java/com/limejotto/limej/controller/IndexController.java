2c0427a3aa21b1d6c1a0c3e6b5f4b92410353254
Varun Batra <varun.batra@stonybrook.edu>
2019-02-20 17:28:14 -0500

    game page

--- src/main/java/com/limejotto/limej/controller/IndexController.java
+++ src/main/java/com/limejotto/limej/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -27,5 +28,4 @@ public class IndexController {
     public String game(Model model){
         return "game";
     }
-
 }
