07d766d7f2eb1bec581014379f7b33c1383c6576
Spencer X <spencerbinxia@gmail.com>
2019-02-20 18:26:47 -0500

    Commit: WordsAPI call 5-letter Regex

--- src/main/resources/static/js/wordSearch.js
+++ src/main/resources/static/js/wordSearch.js
@@ -1,19 +1,26 @@
 $(document).ready(function()
 {
+    var wordbank;
     $.ajax({
         type: "GET",
-        url: "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
+        url: "https://wordsapiv1.p.rapidapi.com/words/?letterPattern=^[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z]$",
         headers: {"X-RapidAPI-Key": "993b4d7194msh67d124ac55fcfafp1969a1jsn8fba7d647451"},
         success: function(wordData)
         {
             console.log(wordData);
             console.log(wordData.body);
             console.log(wordData.status);
-            //console.log(wordData.body);
+            wordbank = wordData;
 
         },
         error: function(e){
             console.log("Failure", e);
         }
     })
+
+    setTimeout(wordTest, 5000);
+
+    function wordTest()
+    {console.log(wordbank);}
 });
+
