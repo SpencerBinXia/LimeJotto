97451d69b229914c37fe4bfcc65d5e865b036f6a
trimartin12 <trimartin12@users.noreply.github.com>
2019-02-27 18:47:16 -0500

    cpu init word has no duplicates

--- src/main/java/com/limejotto/limej/service/wordService.java
+++ src/main/java/com/limejotto/limej/service/wordService.java
@@ -9,6 +9,12 @@ import com.limejotto.limej.repository.wordRepo;
 
 import com.limejotto.limej.object.Word;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @Service
 public class wordService {
@@ -31,13 +37,26 @@ public class wordService {
     }
 
     public String randomWord() {
-        String wRepoResult = wrepo.randomInitialWord();
-        return wRepoResult;
+        List<Word> cpuWords = wrepo.guesstheWord();
+        ArrayList<String> matchedStrings = new ArrayList<>();
+        String regex = "^(?!.*(.).*\\1)[abcdefghijklmnopqrstuvwxyz]+$";
+        Pattern regexP = Pattern.compile(regex);
+        for (Word word : cpuWords) {
+            Matcher matcher = regexP.matcher(word.getWord());
+            if (matcher.matches()) {
+                matchedStrings.add(word.getWord());
+                System.out.println(word.getWord());
+            }
+        }
+        Random rand = new Random();
+        int random = rand.nextInt(matchedStrings.size());
+        return matchedStrings.get(random);
     }
 
 
     public Word guessWordService(String regex)
     {
-        return wrepo.guesstheWord(regex);
+        Word word = new Word();
+        return word; //wrepo.guesstheWord(regex);
     }
 }
