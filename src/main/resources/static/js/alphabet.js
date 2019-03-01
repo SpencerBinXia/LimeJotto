ab057d656b1f577af9e5642a15e9ada113cdd17b
trimartin12 <trimartin12@users.noreply.github.com>
2019-03-01 15:42:11 -0500

    letter color testing in progress + styled modal

--- src/main/resources/static/js/alphabet.js
+++ src/main/resources/static/js/alphabet.js
@@ -8,4 +8,11 @@ let colors = ["#CC0000", "#00CC00", "#000000"];
 var changeColor = function (letter) {
     letter.colorIdx = letter.colorIdx || 0;
     letter.style.color = colors[letter.colorIdx++ % colors.length];
+    replaceColor(letter);
+};
+
+function replaceColor(letter) {
+    var str = document.getElementById("demo").innerHTML;
+    var res = str.replace(/a/gi, '<span style="color: red;">' + letter + '</span>');
+    document.getElementById("demo").innerHTML = res;
 };
